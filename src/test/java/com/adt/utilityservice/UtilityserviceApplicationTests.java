bda203eaf67f2a341453e8216c640d9622a405dc
kavitayadav99 <kavitayadav.adt@gmail.com>
2024-08-09 17:07:14 +0530

    New Email Utility service implemented

---
+++ src/test/java/com/adt/utilityservice/UtilityserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adt.utilityservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UtilityserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
